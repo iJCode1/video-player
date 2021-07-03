a90ed2d14468e8fca930293fffd7d903e88c0427
Joel <joeldome17@gmail.com>
2021-07-03 16:30:26 -0500

    Video Progress Bar Added

--- js/index.js
+++ js/index.js
@@ -2,12 +2,16 @@ const $video = document.querySelector("#video");
 const $forward = document.querySelector("#forward");
 const $play = document.querySelector("#play");
 const $pause = document.querySelector("#pause");
-const  $backward = document.querySelector("#backward");
+const $backward = document.querySelector("#backward");
+const $progress = document.querySelector("#progress");
 
 $play.addEventListener('click', handlePlay);
 $pause.addEventListener('click', handlePause);
 $forward.addEventListener('click', handleForward);
 $backward.addEventListener('click', handleBackward);
+$video.addEventListener('loadedmetadata', handleLoaded);
+$video.addEventListener('timeupdate', handleTimeUpdate);
+$progress.addEventListener('input', handleInput);
 
 function handlePlay(){
     $video.play();
@@ -27,4 +31,16 @@ function handleForward(){
 
 function handleBackward(){
     $video.currentTime-=10;
+}
+
+function handleLoaded(){
+    $progress.max = $video.duration;
+}
+
+function handleTimeUpdate(){
+    $progress.value=$video.currentTime;
+}
+
+function handleInput(){
+    $video.currentTime = $progress.value;
 }
